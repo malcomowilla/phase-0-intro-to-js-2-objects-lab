a2637a0068e38e2d0d3628681ca247c979db504a
malcomowilla <owillamalcom@gmail.com>
2022-11-23 09:19:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+function updateEmployeeWithKeyAndValue(){
+    return(...employee.["name"]: "sam, ["streetAddress"]:
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    employee.name = "Sam",
+    employee.streetAddress = "12 Broadway."
+    return employee;
+}
+function deleteFromEmployeeByKey(){
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+}
+deleteFromEmployeeByKey();
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name
+    return employee;
+}
