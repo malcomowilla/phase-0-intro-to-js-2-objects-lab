64005d8e4a78e18b163534ddc7b2ce28d5c5547d
malcomowilla <owillamalcom@gmail.com>
2023-04-09 15:30:16 +0300

    finished

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 // Write your solution in this file!
-function updateEmployeeWithKeyAndValue(){
-    return(...employee.["name"]: "sam, ["streetAddress"]:
-}
-function destructivelyUpdateEmployeeWithKeyAndValue(){
-    employee.name = "Sam",
-    employee.streetAddress = "12 Broadway."
-    return employee;
-}
-function deleteFromEmployeeByKey(){
-    const newEmployee = {...employee};
-    delete newEmployee.name;
-    return newEmployee;
-}
-deleteFromEmployeeByKey();
+// function updateEmployeeWithKeyAndValue(){
+//     return(...employee.["name"]: "sam, ["streetAddress"]:
+// }
+// function destructivelyUpdateEmployeeWithKeyAndValue(){
+//     employee.name = "Sam",
+//     employee.streetAddress = "12 Broadway."
+//     return employee;
+// }
+// function deleteFromEmployeeByKey(){
+//     const newEmployee = {...employee};
+//     delete newEmployee.name;
+//     return newEmployee;
+// }
+// deleteFromEmployeeByKey();
 
-function destructivelyDeleteFromEmployeeByKey(employee, key){
-    delete employee.name
-    return employee;
+// function destructivelyDeleteFromEmployeeByKey(employee, key){
+//     delete employee.name
+//     return employee;
+// }
+
+const employee = {}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = {...obj};
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+ 
+    
+function deleteFromEmployeeByKey(employee,key ) {
+    const cloneEmployee = {...employee};
+    delete cloneEmployee[key]
+    return cloneEmployee
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
 }
